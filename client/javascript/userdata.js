b03d327774855796fab3ac78d5631e4866bb221d
Edward <ejb07.edward@gmail.com>
2022-08-08 23:16:50 +0100

    Quick change for the Discord URL maker

--- client/javascript/userdata.js
+++ client/javascript/userdata.js
@@ -1,5 +1,5 @@
 window.onload = () => {
-    document.getElementById('login').href = "https://discord.com/api/oauth2/authorize?client_id=732263433579397151&redirect_uri=" + window.location.protocol.slice(0,-1) + "%3A%2F%2F" + window.location.hostname +"%3A" + window.location.port + "%2Foauth2&response_type=token&scope=identify%20guilds" + (window.location.pathname ? "&state=" + window.location.pathname:"")
+    document.getElementById('login').href = "https://discord.com/api/oauth2/authorize?client_id=732263433579397151&redirect_uri=" + window.location.protocol.slice(0,-1) + "%3A%2F%2F" + window.location.hostname +(window.location.port? "%3A" + window.location.port:"") + "%2Foauth2&response_type=token&scope=identify%20guilds" + (window.location.pathname ? "&state=" + window.location.pathname:"")
     function g(cname) {let name = cname + "=";let decodedCookie = decodeURIComponent(document.cookie);let ca = decodedCookie.split(';');for(let i = 0; i <ca.length; i++) {let c = ca[i];while (c.charAt(0) == ' ') {c = c.substring(1);}if (c.indexOf(name) == 0) {return c.substring(name.length, c.length);}}return "";}
 
 
