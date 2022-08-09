fa1c317abae1471f61dc461a2c411af5b029556d
Edward <ejb07.edward@gmail.com>
2022-08-09 19:23:13 +0100

    Removing unused cookies

--- client/javascript/userdata.js
+++ client/javascript/userdata.js
@@ -3,14 +3,14 @@ window.onload = () => {
     function g(cname) {let name = cname + "=";let decodedCookie = decodeURIComponent(document.cookie);let ca = decodedCookie.split(';');for(let i = 0; i <ca.length; i++) {let c = ca[i];while (c.charAt(0) == ' ') {c = c.substring(1);}if (c.indexOf(name) == 0) {return c.substring(name.length, c.length);}}return "";}
 
 
-    const [accessToken, tokenType] = [g("access_token"), g("token_type")];
+    const accessToken = g("access_token");
     if (!accessToken) {
         return (document.getElementById('login').style.display = 'block');
     }
 
     fetch('https://discord.com/api/users/@me', {
         headers: {
-            authorization: `${tokenType} ${accessToken}`,
+            authorization: `Bearer ${accessToken}`,
         },
     })
     .then(result => result.json())
@@ -24,7 +24,7 @@ window.onload = () => {
     fetch(window.location.protocol + "//" + window.location.host + '/api/roblox/users/', {
             headers: {
                 type: `discord`,
-                authorization: `${tokenType} ${accessToken}`,
+                authorization: `Bearer ${accessToken}`,
             },
         })
         .then(result => result.json())
